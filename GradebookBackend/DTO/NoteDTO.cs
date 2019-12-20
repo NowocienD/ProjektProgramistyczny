1ed7358c50aa78e0608fab96472c1752e278a352
unknown <mateusz.czrnecki14@gmail.com>
2019-12-20 19:53:01 +0100

    dodano klasy NoteDTO, noteListDTO oraz StudentService oraz zaimplementowano metodę GetStudenNotesById ktorej nazwa mowi sama za siebie

---
+++ GradebookBackend/DTO/NoteDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradebookBackend.DTO
+{
+    public class NoteDTO
+    {
+        public string statement { get; set; }
+        public string teacherFirstName { get; set; }
+        public string teacherSurname { get; set; }
+
+    }
+}
---
+++ GradebookBackend/Services/StudentService.cs
@@ -0,0 +1,39 @@
+﻿using GradebookBackend.DTO;
+using GradebookBackend.Model;
+using GradebookBackend.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradebookBackend.Services
+{
+    public class StudentService
+    {
+        private readonly IRepository<Student> studentRepository;
+
+        public StudentService(IRepository<Student> studentRepository)
+        {
+            this.studentRepository = studentRepository;
+        }
+
+        public NoteListDTO GetStudentNotesByID(int id)
+        {
+            NoteListDTO noteListDTO = new NoteListDTO();
+            List<Note> studentNoteList = studentRepository.Get(id).Notes;
+
+            NoteDTO noteDTO;
+            foreach(Note note in studentNoteList)
+            {
+                noteDTO = new NoteDTO
+                {
+                    statement = note.statement,
+                    teacherFirstName = note.Teacher.User.Firstname,
+                    teacherSurname = note.Teacher.User.Surname
+                };
+                noteListDTO.NoteDTOs.Add(noteDTO);
+            }
+            return noteListDTO;
+        }
+    }
+}
