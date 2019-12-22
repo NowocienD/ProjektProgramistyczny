b7521e5cbe1cf4ab6577bb9d3cb0887b70709c30
unknown <mateusz.czrnecki14@gmail.com>
2019-12-22 12:07:45 +0100

    dodano StudentController i IStudentService ktorego brakowalo

---
+++ GradebookBackend/ServicesCore/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using GradebookBackend.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradebookBackend.ServicesCore
+{
+    interface IStudentService
+    {
+        public NoteListDTO GetStudentNotesByID(int id);
+    }
+}
