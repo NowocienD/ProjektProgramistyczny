9dbe7d0b93c521dfed990cadfd8c7d693ec20ecf
Mateusz Czarnecki <mateusz.czarnecki14@gmail.com>
2019-12-28 18:39:46 +0100

    Dodano klasę SubjectController

---
+++ GradebookBackend/Controllers/SubjectController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradebookBackend.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class SubjectController : Controller
+    {
+
+    }
+}
