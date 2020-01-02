82d7babc737597425f1a64ee3f3224d7ff7e7bca
Mateusz Czarnecki <mateusz.czarnecki14@gmail.com>
2020-01-02 16:43:24 +0100

    dodano klasy StudentDTO i StudentListDTO

---
+++ GradebookBackend/DTO/StudentListDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradebookBackend.DTO
+{
+    public class StudentListDTO
+    {
+        public List<StudentDTO> studentList { get; set; }
+
+        public StudentListDTO()
+        {
+            studentList = new List<StudentDTO>();
+        }
+    }
+}
