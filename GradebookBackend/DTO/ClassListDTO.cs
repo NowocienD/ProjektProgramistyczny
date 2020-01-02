c4443496a5a99832d7f5af6d4692271fcafa332f
Mateusz Czarnecki <mateusz.czarnecki14@gmail.com>
2020-01-02 15:18:52 +0100

    Dodano klase ClassListDTO

---
+++ GradebookBackend/DTO/ClassListDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradebookBackend.DTO
+{
+    public class ClassListDTO
+    {
+        public List<ClassDTO> ClassDTOs { get; set; }
+
+        public ClassListDTO()
+        {
+            ClassDTOs = new List<ClassDTO>();
+        }
+    }
+}
