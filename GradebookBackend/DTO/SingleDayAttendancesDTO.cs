3be8baf0d1ad148507a19405f8382a33c5d7aee2
nalepa_michal <33619102+Nalepa@users.noreply.github.com>
2020-01-05 14:32:57 +0100

    Dodanie klas DTO to zwracania planu obecnosci

---
+++ GradebookBackend/DTO/SingleDayAttendancesDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradebookBackend.DTO
+{
+    public class SingleDayAttendances
+    {
+        private readonly int maxNumberOfLessonForDay = 8;
+
+        public string Date { get; set; }
+        public int DayOfTheWeek { get; set; }
+        public List<string> Attendances { get; set; }
+
+        public SingleDayAttendance()
+        {
+            Attendances = new List<string>();
+            for(int i = 0; i < maxNumberOfLessonForDay; i++)
+            {
+                Attendances.Add("Not entered");
+            }
+        }
+    }
+}
