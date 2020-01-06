b0e49f8867b6bb4b36845fad672e3f1d1d33f904
nalepa_michal <33619102+Nalepa@users.noreply.github.com>
2020-01-06 19:48:10 +0100

    zamiana dto na url przy obecnosciach

--- GradebookBackend/ServicesCore/IAttendanceService.cs
+++ GradebookBackend/ServicesCore/IAttendanceService.cs
@@ -8,6 +8,6 @@ namespace GradebookBackend.ServicesCore
 {
     public interface IAttendanceService
     {
-        public SingleDayAttendancesListDTO GetAttendancesByStudentId(int studentId, DatesDTO datesDTO);
+        public SingleDayAttendancesListDTO GetAttendancesByStudentId(int studentId, string date);
     }
 }
