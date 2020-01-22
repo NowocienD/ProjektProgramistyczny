b2a4ac4c883ebc2bd9d3b42cc12862e43fb22073
nalepa_michal <33619102+Nalepa@users.noreply.github.com>
2020-01-22 22:36:22 +0100

    Dodanie zwracania teachersubject dla subjectId

---
+++ GradebookBackend/Controllers/TeacherSubjectController.cs
@@ -0,0 +1,43 @@
+﻿using GradebookBackend.DTO;
+using GradebookBackend.ServicesCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradebookBackend.Controllers
+{
+    [ApiController]
+    [Route("api/teachersubject")]
+    public class TeacherSubjectController : ControllerBase
+    {
+        private readonly IUserProviderService userProviderService;
+        private readonly IUserService userService;
+        private readonly ITeacherSubjectService teacherSubjectService;
+        public TeacherSubjectController(IUserProviderService userProviderService, IUserService userService,
+            ITeacherSubjectService teacherSubjectService)
+        {
+            this.userProviderService = userProviderService;
+            this.userService = userService;
+            this.teacherSubjectService = teacherSubjectService;
+        }
+
+        [Authorize]
+        [HttpGet("admin/allteachersubject/{subjectId}")]
+        public IActionResult GetTeacherSubjects(int subjectId)
+        {
+            int userId = int.Parse(userProviderService.GetUserId());
+            if(userService.IsAdmin(userId))
+            {
+                TeacherSubjectListDTO teacherSubjectListDTO = teacherSubjectService.GetTeacherSubjectBySubjectId(subjectId);
+                return Ok(teacherSubjectListDTO);
+            }
+            else
+            {
+                return BadRequest("Brak uprawnien administratora");
+            }        
+        }
+    }
+}
