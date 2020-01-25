6d781723f39b1aa5f602061abedaa3c36024365f
nalepa_michal <33619102+Nalepa@users.noreply.github.com>
2020-01-26 00:18:02 +0100

    Dodanie modyfikacji studenta

--- GradebookBackend/Repositories/StudentRepository.cs
+++ GradebookBackend/Repositories/StudentRepository.cs
@@ -45,6 +45,7 @@ namespace GradebookBackend.Repositories
 
         public StudentDAO Update(StudentDAO tObjectChanges)
         {
+            context.Entry(tObjectChanges).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
             var tObject = this.context.Students.Attach(tObjectChanges);
             tObject.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
             this.context.SaveChanges();
