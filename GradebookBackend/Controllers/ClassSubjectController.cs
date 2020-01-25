d7be1d8320961268b5b13ae46e226dc90577f307
nalepa_michal <33619102+Nalepa@users.noreply.github.com>
2020-01-25 19:01:15 +0100

    Dodawanie i usuwanie powiazan klasy z przedmiotem

---
+++ GradebookBackend/Controllers/ClassSubjectController.cs
@@ -0,0 +1,59 @@
+﻿using GradebookBackend.ServicesCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradebookBackend.Controllers
+{
+    [ApiController]
+    [Route("api/classsubject/")]
+    public class ClassSubjectController : ControllerBase
+    {
+        private readonly IUserProviderService userProviderService;
+        private readonly IUserService userService;
+        private readonly IClassSubjectService classSubjectService;
+
+        public ClassSubjectController(IUserProviderService userProviderService, IUserService userService,
+            IClassSubjectService classSubjectService)
+        {
+            this.userProviderService = userProviderService;
+            this.userService = userService;
+            this.classSubjectService = classSubjectService;
+        }
+
+        [Authorize]
+        [HttpGet("admin/addclasssubject")]
+        public IActionResult AddClassSubject([FromQuery] int classId, [FromQuery] int subjectId)
+        {
+            int userId = int.Parse(userProviderService.GetUserId());
+            if (userService.IsAdmin(userId))
+            {
+                classSubjectService.AddClassSubject(classId, subjectId);
+                return Ok("Pomyslnie dodano powiazanie klasy z przedmiotem");
+            }
+            else
+            {
+                return BadRequest("Brak uprawnien administratora");
+            }
+        }
+
+        [Authorize]
+        [HttpDelete("admin/deleteclasssubject")]
+        public IActionResult DeleteClassSubject([FromQuery] int classId, [FromQuery] int subjectId)
+        {
+            int userId = int.Parse(userProviderService.GetUserId());
+            if (userService.IsAdmin(userId))
+            {
+                classSubjectService.DeleteClassSubject(classId, subjectId);
+                return Ok("Pomyslnie usunieto powiazanie klasy z przedmiotem");
+            }
+            else
+            {
+                return BadRequest("Brak uprawnien administratora");
+            }
+        }
+    }
+}
---
+++ GradebookBackend/Services/ClassSubjectService.cs
@@ -0,0 +1,32 @@
+﻿using GradebookBackend.Model;
+using GradebookBackend.Repositories;
+using GradebookBackend.ServicesCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradebookBackend.Services
+{
+    public class ClassSubjectService : IClassSubjectService
+    {
+        public IRepositoryRelation<ClassSubjectDAO> classSubjectRepository;
+        public ClassSubjectService(IRepositoryRelation<ClassSubjectDAO> classSubjectRepository)
+        {
+            this.classSubjectRepository = classSubjectRepository;
+        }
+        public void AddClassSubject(int classId, int subjectId)
+        {
+            ClassSubjectDAO newClassSubjectDAO = new ClassSubjectDAO
+            {
+                ClassId = classId,
+                SubjectId = subjectId
+            };
+            classSubjectRepository.Add(newClassSubjectDAO);
+        }
+        public void DeleteClassSubject(int classId, int subjectId)
+        {
+            classSubjectRepository.Delete(classId, subjectId);
+        }
+    }
+}
