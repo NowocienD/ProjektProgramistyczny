d720fb48995df54182e9692c3deeb61735e4c314
Dominik <dominik.nowocien@gmail.com>
2020-09-22 17:25:28 +0200

    nadpisanie metody equals

--- GradebookBackend/DTO/ClassDTO.cs
+++ GradebookBackend/DTO/ClassDTO.cs
@@ -5,5 +5,19 @@ namespace GradebookBackend.DTO
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ClassDTO)obj);
+        }
+
+        public bool Equals(ClassDTO other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Id == other.Id && string.Equals(Name, other.Name);
+        }
     }
 }
