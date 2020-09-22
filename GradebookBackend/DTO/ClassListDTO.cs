1109106b3a32ec369f296591dc4831539de6340a
Dominik <dominik.nowocien@gmail.com>
2020-09-22 17:42:10 +0200

    dodanie metody equals

--- GradebookBackend/DTO/ClassListDTO.cs
+++ GradebookBackend/DTO/ClassListDTO.cs
@@ -10,5 +10,24 @@ namespace GradebookBackend.DTO
         {
             ClassList = new List<ClassDTO>();
         }
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ClassListDTO)obj);
+        }
+
+        public bool Equals(ClassListDTO other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            for (int i = 0; i < ClassList.Count; i++)
+            {
+                if (!ClassList[i].Equals(other.ClassList[i]))
+                    return false;
+            }
+            return true;
+        }
     }
 }
